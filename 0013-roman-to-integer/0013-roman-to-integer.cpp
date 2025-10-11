c89cd64112edc34bcdf73b567bbfe86460fd603e
zakaria sersoub <76943955+zakariasersoub@users.noreply.github.com>
2025-10-11 21:34:04 +0100

    Time: 0 ms (100%), Space: 10 MB (86.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> roman = {
-            {'I',1}, {'V',5}, {'X',10}, {'L',50},
-            {'C',100}, {'D',500}, {'M',1000}
-        };
-        
-        int result = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            int curr = roman[s[i]];
-            int next = (i+1 < s.size()) ? roman[s[i+1]] : 0;
-            
-            if (curr < next) {
-                result -= curr;
-            } else {
-                result += curr;
+        int counter = 0;
+        for (int i = 0; i < s.size(); i++) {
+            int current = 0;
+            int next = 0;
+
+            if (s[i] == 'I') current = 1;
+            else if (s[i] == 'V') current = 5;
+            else if (s[i] == 'X') current = 10;
+            else if (s[i] == 'L') current = 50;
+            else if (s[i] == 'C') current = 100;
+            else if (s[i] == 'D') current = 500;
+            else if (s[i] == 'M') current = 1000;
+
+            if (i + 1 < s.size()) {
+                if (s[i + 1] == 'I') next = 1;
+                else if (s[i + 1] == 'V') next = 5;
+                else if (s[i + 1] == 'X') next = 10;
+                else if (s[i + 1] == 'L') next = 50;
+                else if (s[i + 1] == 'C') next = 100;
+                else if (s[i + 1] == 'D') next = 500;
+                else if (s[i + 1] == 'M') next = 1000;
             }
+
+            if (current < next)
+                counter -= current;
+            else
+                counter += current;
         }
-        return result;
+        return counter;
     }
 };
